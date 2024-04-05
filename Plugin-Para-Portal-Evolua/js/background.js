d9ddae565341f5fa356dab924297a7a2c4f5d29e
Marcos Tomaz <marcos1tomaz2@gmail.com>
2024-04-04 21:36:33 -0300

    Source update

--- Plugin-Para-Portal-Evolua/js/background.js
+++ Plugin-Para-Portal-Evolua/js/background.js
@@ -27,7 +27,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         return undefined;
     }
 
-
+    //Load the user preferences and run the optional functions
+    chrome.storage.sync.get(['enableLogo', 'closeTabs'], function (data) {
+        ChangePageLogoToEnsinext(data.enableLogo, tabId);
+        CloseOtherTabs(data.closeTabs);
+    });
 
     //Reset the zoom of the page (to 100%)
     chrome.tabs.setZoom(tabId, 1.0, null);
@@ -59,6 +63,72 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         console.log("New page detected. Post Processing...");
     });
 });
+
+//Function that changes the page logo to Ensinext
+function ChangePageLogoToEnsinext(logoEnabled, tabId) {
+    //If the logo is not enabled, ignore this call
+    if (logoEnabled != "true")
+        return;
+
+    //Run the logo change script in the page context
+    chrome.scripting.executeScript({
+        target: { tabId: tabId }, func: () => {
+            //Try to change the favicon
+            var linkNodes = document.getElementsByTagName("HEAD")[0].getElementsByTagName("LINK");
+            //Change the icon URL for all favicon tags
+            for (var i = 0; i < linkNodes.length; i++) {
+                //If is not a tag of favicon, ignore
+                if (linkNodes[i].getAttribute("rel") != "icon")
+                    continue;
+
+                //Change the favicon
+                linkNodes[i].setAttribute("href", chrome.runtime.getURL("img/ensinext-favicon.png"));
+            }
+
+            //Try to change the logo of login screen
+            var loginLogoNode = document.getElementsByClassName("logo-login")[0];
+            if (loginLogoNode != null && loginLogoNode != undefined)
+                loginLogoNode.src = chrome.runtime.getURL("img/ensinext-login.png");
+
+            //Try to change the nav bar icon
+            var navbarLogoNode = document.getElementsByClassName("logo-nav")[0];
+            if (navbarLogoNode != null && navbarLogoNode != undefined)
+                navbarLogoNode.src = chrome.runtime.getURL("img/ensinext-navbar.png");
+        }
+    }).then(() => {
+        console.log("New page detected. Changing Logos...");
+    });
+}
+
+//Function that process the close other thabs that is not portal evolua
+function CloseOtherTabs(closeTabsEnabled) {
+    //If the close other tabs is not enabled, ignore this call
+    if (closeTabsEnabled != "true")
+        return;
+
+    //Query all currently opened tabs (in all windows) and close all that don't have portal evolua site
+    chrome.tabs.query({ windowType: "normal" }, function (resultTabArray) {
+        //Prepare the information about tabs
+        var hasClosedTabs = false;
+
+        //Check each tab...
+        for (var i = 0; i < resultTabArray.length; i++)
+            if (resultTabArray[i].url?.includes(chrome.runtime.getManifest().host_permissions[0].replace("*://*.", "").replace("/*", "")) == false) {
+                chrome.tabs.remove(resultTabArray[i].id, function () { });
+                hasClosedTabs = true;
+            }
+
+        //If was closed some tab, warn it
+        if (hasClosedTabs == true)
+            chrome.notifications.create("tabWarn",
+                {
+                    type: "basic",
+                    iconUrl: "../img/icon-64px.png",
+                    title: "Abas Fechadas",
+                    message: "As outras abas do Browser, foram fechadas automaticamente. Mantenha o foco!",
+                }, function () { });
+    });
+}
 
 //Function that post process the code injection on the loaded page
 function PostProcessLoadedPage() {
---
+++ Plugin-Para-Portal-Evolua/js/popup.js
@@ -0,0 +1,60 @@
+// @ts-nocheck
+
+//Private variables
+//...
+
+//------ Core functions ------
+
+//Function that load the user preferences to UI
+function LoadUserPreferences() {
+    //Find the UI elements
+    var set_enableLogo = document.getElementById("set_enableLogo");
+    var set_closeTabs = document.getElementById("set_closeTabs");
+
+    //If don't have a preference for "enableLogo"
+    if (localStorage.getItem("enableLogo") == null) {
+        //Set default option for "enableLogo"
+        localStorage.setItem("enableLogo", "false");
+        set_enableLogo.value = "false";
+    }
+
+    //If don't have a preference for "closeTabs"
+    if (localStorage.getItem("closeTabs") == null) {
+        //Set default option for "closeTabs"
+        localStorage.setItem("closeTabs", "true");
+        set_closeTabs.value = "true";
+    }
+
+    //Load the preferences to UI
+    set_enableLogo.value = localStorage.getItem("enableLogo");
+    set_closeTabs.value = localStorage.getItem("closeTabs");
+}
+
+//Function that install the on change listener to options
+function InstallAutoSaveListenersForPrefs() {
+    //Find the UI elements
+    var set_enableLogo = document.getElementById("set_enableLogo");
+    var set_closeTabs = document.getElementById("set_closeTabs");
+
+    //Install the callbacks
+    set_enableLogo.addEventListener("change", function () {
+        //Save the preference for UI
+        localStorage.setItem("enableLogo", set_enableLogo.value);
+
+        //Save the preference for Background
+        chrome.storage.sync.set({ "enableLogo": set_enableLogo.value });
+    });
+    set_closeTabs.addEventListener("change", function () {
+        //Save the preference for UI
+        localStorage.setItem("closeTabs", set_closeTabs.value);
+
+        //Save the preference for Background
+        chrome.storage.sync.set({ "closeTabs": set_closeTabs.value });
+    });
+}
+
+//Load the current user preferences on end of the DOM creation
+window.onload = function () {
+    LoadUserPreferences();
+    InstallAutoSaveListenersForPrefs();
+}
