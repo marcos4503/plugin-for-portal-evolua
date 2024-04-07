24f2a3a250e5440f105f2909e19c674a725ff9f5
Marcos Tomaz <marcos1tomaz2@gmail.com>
2024-04-07 10:06:49 -0300

    Source update

--- Plugin-Para-Portal-Evolua/js/popup.js
+++ Plugin-Para-Portal-Evolua/js/popup.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 
 //Private variables
-//...
+var currentExistingToastTimeout = null;
 
 //------ Core functions ------
 
@@ -11,23 +11,26 @@ function LoadUserPreferences() {
     var set_enableLogo = document.getElementById("set_enableLogo");
     var set_closeTabs = document.getElementById("set_closeTabs");
 
-    //If don't have a preference for "enableLogo"
-    if (localStorage.getItem("enableLogo") == null) {
-        //Set default option for "enableLogo"
-        localStorage.setItem("enableLogo", "false");
-        set_enableLogo.value = "false";
-    }
+    //Try to load the current preferences of optional functions
+    chrome.storage.sync.get(['enableLogo', 'closeTabs'], function (data) {
+        //If don't have a preference for "enableLogo", set default
+        if (data.enableLogo == null)
+            chrome.storage.sync.set({ "enableLogo": "false" });
+        //If don't have a preference for "closeTabs", set default
+        if (data.closeTabs == null)
+            chrome.storage.sync.set({ "closeTabs": "true" });
 
-    //If don't have a preference for "closeTabs"
-    if (localStorage.getItem("closeTabs") == null) {
-        //Set default option for "closeTabs"
-        localStorage.setItem("closeTabs", "true");
-        set_closeTabs.value = "true";
-    }
+        //Get the updated current preferences
+        chrome.storage.sync.get(['enableLogo', 'closeTabs'], function (data) {
+            //Render the current preferences in the UI
+            set_enableLogo.value = data.enableLogo;
+            set_closeTabs.value = data.closeTabs;
 
-    //Load the preferences to UI
-    set_enableLogo.value = localStorage.getItem("enableLogo");
-    set_closeTabs.value = localStorage.getItem("closeTabs");
+            //Unlock the edition of fields
+            set_enableLogo.removeAttribute("disabled");
+            set_closeTabs.removeAttribute("disabled");
+        });
+    });
 }
 
 //Function that install the on change listener to options
@@ -38,18 +41,18 @@ function InstallAutoSaveListenersForPrefs() {
 
     //Install the callbacks
     set_enableLogo.addEventListener("change", function () {
-        //Save the preference for UI
-        localStorage.setItem("enableLogo", set_enableLogo.value);
-
-        //Save the preference for Background
+        //Save the preference
         chrome.storage.sync.set({ "enableLogo": set_enableLogo.value });
+
+        //Notify
+        ShowToastNotification("Configuração aplicada!");
     });
     set_closeTabs.addEventListener("change", function () {
-        //Save the preference for UI
-        localStorage.setItem("closeTabs", set_closeTabs.value);
-
-        //Save the preference for Background
+        //Save the preference
         chrome.storage.sync.set({ "closeTabs": set_closeTabs.value });
+
+        //Notify
+        ShowToastNotification("Configuração aplicada!");
     });
 }
 
@@ -64,4 +67,37 @@ window.onload = function () {
     LoadUserPreferences();
     InstallAutoSaveListenersForPrefs();
     RenderExtensionVersion();
+}
+
+
+
+
+
+//------ Auxiliar functions ------
+
+//Function that show a toast notification
+function ShowToastNotification(message) {
+    //Get the element of toasts
+    var toastContainer = document.getElementById("toastContainer");
+
+    //Set the message
+    toastContainer.innerHTML = message;
+
+    //Show the toast
+    toastContainer.setAttribute("style", "bottom: 0px;");
+
+    //If have a toast timeout existing, clear it
+    if (currentExistingToastTimeout != null) {
+        window.clearTimeout(currentExistingToastTimeout);
+        currentExistingToastTimeout = null;
+    }
+
+    //Wait time and auto close the message
+    currentExistingToastTimeout = window.setTimeout(function () {
+        //Hide the toast
+        toastContainer.removeAttribute("style");
+
+        //Clear the toast timeout reference
+        currentExistingToastTimeout = null;
+    }, 1500);
 }
